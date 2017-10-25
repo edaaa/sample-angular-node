053ead8165eecd72f3aeba78b3fc635709cb8930
edaaa <eesmekaya17@outlook.com>
2017-10-26 01:04:07 +0300

    deneme
    deneme

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: '8UxAzRCmQJTruSJh18BsvK6AK',
+		consumer_secret: 'W7kEvpqjFBOXex3PV0wZ5FNZWl0nYcFftaXFZqUaGq6KRkeQPv',
+		access_token: '912025145941819392-EE6tCf6ukrUO1wBDvJ6gZbA8h7z4P7e',
+		access_token_secret: 'owMmWOmjAdmYeM90iG4YcNlv0svE5rNeJ1yBtvOORWTGD'
+	}
+
+};
