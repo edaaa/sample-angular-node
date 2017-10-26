0096c6c7c84bc085aa143be304155ce7a511e352
edaaa <eesmekaya17@outlook.com>
2017-10-26 23:28:34 +0300

    demo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Angular, Node and Twitter API' });
+  res.render('index', { title: 'Angular,  Twitter API' });
 });
 
 module.exports = router;
